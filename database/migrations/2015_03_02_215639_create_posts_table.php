ec6a07fce83e7891e9d6778545fb4a7a521aa534
bound1ess <bound1ess.dev@gmail.com>
2015-03-02 22:03:45 +0600

    Add migration for Posts.

---
+++ database/migrations/2015_03_02_215639_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('posts', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->text('contents');
+            $table->boolean('is_important');
+            $table->integer('category_id')->unsigned();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('posts');
+	}
+
+}
