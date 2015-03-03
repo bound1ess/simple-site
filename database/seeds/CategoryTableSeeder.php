426b1f31227af66ac844439db5a756765442421a
bound1ess <bound1ess.dev@gmail.com>
2015-03-03 22:01:13 +0600

    Update the seeder.

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
 class CategoryTableSeeder extends Seeder {
 
@@ -10,26 +9,27 @@ class CategoryTableSeeder extends Seeder {
      */
     public function run()
     {
-        $faker = Faker::create();
+        $categories = [
+            [
+                "name" => "Foo",
+                "parent_id" => null,
+            ],
+            [
+                "name" => "Bar",
+                "parent_id" => 1,
+            ],
+            [
+                "name" => "Baz",
+                "parent_id" => 1,
+            ],
+            [
+                "name" => "Fizz",
+                "parent_id" => 3,
+            ],
+        ];
 
-        // Add a bunch of categories.
-        for ($index = 1; $index <= 30; $index++) {
-            if ($faker->boolean(33)) {
-                // No parent directory.
-                Frostbite\Category::create([
-                    'name' => $faker->sentence(),
-                    'parent_id' => null,
-                ]);
-            } else {
-                do {
-                    $parentId = rand(1, $index);
-                } while ($index !== 1 and $parentId == $index);
-
-                Frostbite\Category::create([
-                    'name' => $faker->sentence(),
-                    'parent_id' => $parentId,
-                ]);
-            }
+        foreach ($categories as $category) {
+            Frostbite\Category::create($category);
         }
     }
 }
