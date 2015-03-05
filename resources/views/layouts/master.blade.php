a55abdbf53aa73867ff43969184f5fde76753c4d
bound1ess <bound1ess.dev@gmail.com>
2015-03-05 19:12:13 +0600

    Display the page title.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
         <title>
             @section('title')
                 {{ Lang::get('messages.project-name') }}
-            @stop
+            @show
         </title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.2/united/bootstrap.min.css">
         <style>
