25d20c620769a6ef24f2476d38309d4c9f825f4d
bound1ess <bound1ess.dev@gmail.com>
2015-03-05 17:25:42 +0600

    Add main-page config.

---
+++ config/main-page.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'description' => 'Our Foobar organisation is so awesome.',
+
+    'address' => 'Berlin, Foobar st. 785206',
+
+    'maintainers' => [
+        // ...
+        [
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'phone' => '+5151616161',
+        ],
+        [
+            'name' => 'Jack Foobar',
+            'email' => 'foo@bar.org',
+            'phone' => '+3141515272',
+        ],
+    ],
+];
