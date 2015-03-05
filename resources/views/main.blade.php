0671a713a178ccd7de283fbd9beb61d92d018b07
bound1ess <bound1ess.dev@gmail.com>
2015-03-05 17:37:43 +0600

    Show info on the main page.

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.master')
 
 @section('body')
-    Hello, world!
+    <p class="lead">{{ Config::get('main-page.description') }}</p>
+    <address><strong>{{ Config::get('main-page.address') }}</strong></address>
+
+    <hr>
+
+    @foreach (Config::get('main-page.maintainers') as $maintainer)
+        <p>{{ $maintainer['name'] }}
+            (<strong>{{ $maintainer['phone'] }}</strong>,
+            <i>{{ $maintainer['email'] }}</i>)
+        </p>
+    @endforeach
 @stop
