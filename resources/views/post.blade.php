e036ce98ae5bd981ad1f4bc2c442bc4fcd9e17bd
bound1ess <bound1ess.dev@gmail.com>
2015-03-13 17:56:50 +0600

    Display Edit button under every post.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,5 +2,14 @@
 
 @section('body')
     <h2>{{ $post->title }}</h2>
-    <p class="lead">{{ $post->contents }}</p>
+
+    <p class="lead">
+        {{ $post->contents }}
+    </p>
+
+    @if (Auth::check())
+        <a href="/posts/{{ $post->id }}/edit" class="btn btn-lg btn-primary">
+            {{ trans('messages.edit') }}
+        </a>
+    @endif
 @stop
