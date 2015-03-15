5311ed156b05b555c121127d250e08cd5b3cfe17
bound1ess <bound1ess.dev@gmail.com>
2015-03-15 13:37:19 +0600

    Move all lang files to lang/en.

--- resources/lang/ru/errors.php
+++ resources/lang/ru/errors.php
@@ -1,11 +1,5 @@
 <?php
 
 return [
-    'login' => 'Your e-mail and/or password are/is incorrect.',
-    'profile' => 'You typed an invalid e-mail and/or password.',
-    'pswd-mismatch' => 'Your ACTUAL (old) password is not correct.',
-    'main-page' => 'The company name or/and slogan is/are incorrect.',
-    'post' => 'You did leave blank some of the fields.',
-    'category' => 'Please fill in the category name.',
-    'no-posts' => 'No posts just yet.',
+    // ...
 ];
--- resources/lang/ru/messages.php
+++ resources/lang/ru/messages.php
@@ -1,44 +1,5 @@
 <?php
 
 return [
-    //'project-name' => 'Foo and Co',
-    //'project-desc' => 'Join our foobar community today!',
-
-    'navigation'     => 'Navigation',
-    'important'      => 'Important',
-    'sub-categories' => 'Sub categories',
-    'category'       => 'Category',
-    'posts'          => 'Posts',
-
-    // Login related.
-    'login'   => 'Log In',
-    'email'   => 'E-mail',
-    'pswd'    => 'Password',
-    'logout'  => 'Log out',
-    'profile' => 'Profile',
-
-    'back-to-site' => 'Back to site',
-
-    // Related to the dashboard.
-    'main'       => 'Main',
-    'categories' => 'Categories',
-    'dashboard'  => 'Dashboard',
-
-    'save'     => 'Save',
-    'old-pswd' => 'Old password',
-    'new-pswd' => 'New password',
-
-    'name' => 'Project name',
-    'desc' => 'The slogan',
-
-    'edit'     => 'Edit',
-    'title'    => 'Title',
-    'contents' => 'Contents',
-    'new'      => 'New',
-
-    'category-name'   => 'Category name',
-    'parent-category' => 'Parent category',
-
-    'placeholder' => 'Type something cool here...',
-    'back' => 'Back',
+    // ...
 ];
