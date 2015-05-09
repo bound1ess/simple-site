25c1bd7089b9bf49b217febdaeac252f5826fc7b
bound1ess <bound1ess.dev@gmail.com>
2015-05-09 21:36:10 +0600

    Display all visible images.

---
+++ resources/lang/en/upload.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'hide' => 'Remove image',
+];
--- resources/views/upload/index.blade.php
+++ resources/views/upload/index.blade.php
@@ -1,5 +1,19 @@
 @extends('admin.dashboard')
 
 @section('dashboard-body')
-    Hello, world!
+    <h3>{{ trans('messages.upload') }}</h3>
+    <!-- Dropzone here? -->
+    <ul>
+        @foreach ($uploads as $upload)
+            <li>
+                <a href="/uploads/{{ urlencode($upload->path) }}" target="_blank">
+                    {{ $upload->name }}
+                </a>
+
+                <a href="/admin/upload/hide?id={{ $upload->id }}">
+                    ({{ trans('upload.hide') }})
+                </a>
+            </li>
+        @endforeach
+    </ul>
 @stop
